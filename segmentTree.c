2088a58f9d303775aa7c479c874c4833f73872df
Michel G. Souza <Michelgomesdes@hotmail.com>
2023-06-26 21:03:23 -0300

    Update segmentTree.c

--- segmentTree.c
+++ segmentTree.c
@@ -32,43 +32,7 @@ int queryOp(SegmentTree st, int node, int start, int end, int left, int right) {
     return leftSum + rightSum;
 }
 
-void buildSegmentTreeUtil(SegmentTree st, int node, int start, int end, int op) {
-    if (start == end) {
-        st.tree[node] = st.arr[start];
-    } else {
-        int mid = getMid(start, end);
-        buildSegmentTreeUtil(st, 2 * node + 1, start, mid, op);
-        buildSegmentTreeUtil(st, 2 * node + 2, mid + 1, end, op);
-        
-        if (op == OP_SOMA) {
-            st.tree[node] = st.tree[2 * node + 1] + st.tree[2 * node + 2];
-        } 
-        else if (op == OP_SUBTRACAO) {
-            st.tree[node] = st.tree[2 * node + 1] - st.tree[2 * node + 2];
-        }
-        else if (op == OP_MULTIPLICACAO) {
-            st.tree[node] = st.tree[2 * node + 1] * st.tree[2 * node + 2];
-        }
-        else if (op == OP_DIVISAO) {
-            st.tree[node] = st.tree[2 * node + 1] / st.tree[2 * node + 2];
-        } 
-        else if (op == OP_MAX) {
-            if (st.tree[2 * node + 1] > st.tree[2 * node + 2]) {
-                st.tree[node] = st.tree[2 * node + 1];     
-            } else {
-                st.tree[node] = st.tree[2 * node + 2];   
-            }
-            printf("Maximo do intervalo [%d, %d]: %d\n", start, end, st.tree[node]);
-        }
-        else if (op == OP_MIN) {
-            if (st.tree[2 * node + 1] < st.tree[2 * node + 2]) {
-                st.tree[node] = st.tree[2 * node + 1];    
-            } else {
-                st.tree[node] = st.tree[2 * node + 2];   
-            }   
-        }
-    }
-    
+void do_print(int op, int start, int end, SegmentTree st, int node){
     if (op == OP_SOMA) {
         printf("Soma do intervalo [%d, %d]: %d\n", start, end, st.tree[node]);
     } 
@@ -89,6 +53,53 @@ void buildSegmentTreeUtil(SegmentTree st, int node, int start, int end, int op) 
     }
 }
 
+void do_operation(SegmentTree st, int node, int op, int start, int end){
+    int val1 = st.tree[2 * node + 1];
+    int val2 = st.tree[2 * node + 2];
+    
+    if (op == OP_SOMA) {
+        st.tree[node] = val1 + val2;
+    } 
+    else if (op == OP_SUBTRACAO) {
+        st.tree[node] = val1 - val2;
+    }
+    else if (op == OP_MULTIPLICACAO) {
+        st.tree[node] = val1 * val2;
+    }
+    else if (op == OP_DIVISAO) {
+        st.tree[node] = val1 / val2;
+    } 
+    else if (op == OP_MAX) {
+        if (val1 >val2) {
+            st.tree[node] = val1;     
+        } else {
+            st.tree[node] = val2;   
+        }
+    }
+    else if (op == OP_MIN) {
+        if (val1 <val2) {
+            st.tree[node] = val1;    
+        } else {
+            st.tree[node] = val2;   
+        }   
+    }
+
+}
+
+void buildSegmentTreeUtil(SegmentTree st, int node, int start, int end, int op) {
+    if (start == end) {
+        st.tree[node] = st.arr[start];
+    } else {
+        int mid = getMid(start, end);
+        buildSegmentTreeUtil(st, 2 * node + 1, start, mid, op);
+        buildSegmentTreeUtil(st, 2 * node + 2, mid + 1, end, op);
+
+        do_operation(st, node, op, start, end);
+        do_print(op, start, end, st, node);
+
+    }
+}
+
 SegmentTree buildSegmentTree(int arr[], int n, int op) {
     SegmentTree st;
     st.arr = arr;
@@ -105,6 +116,7 @@ void defaultInput(){ //Mesmo vetor e operacao do artigo
     printf("* Default input *\n\n");
     
     int arr[] = {1, 3, -2, 8, -7};
+    // int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
     int i;
     
@@ -121,7 +133,13 @@ void defaultInput(){ //Mesmo vetor e operacao do artigo
     SegmentTree st = buildSegmentTree(arr, n, op);
     clock_t end = clock();
     double elapsed_time = ((double) (end - start)) / CLOCKS_PER_SEC;
-    
+
+    printf("Arvore final: \n");
+    for (int i = 0; i <= sizeof(st.tree) ; i++) {
+        printf("%d\t", st.tree[i]);
+    }
+    printf("\n\n");
+
     free(st.tree);
     
     printf("Tempo de execucao: %.6f segundos\n", elapsed_time);
@@ -172,14 +190,19 @@ void customInput(){ //Vetor e operacao a definir
     SegmentTree st = buildSegmentTree(arr, n, op);
     clock_t end = clock();
     double elapsed_time = ((double) (end - start)) / CLOCKS_PER_SEC;
-    
+    printf("Arvore final: \n");
+    for (int i = 0; i <= sizeof(st.tree) ; i++) {
+        printf("%d\t", st.tree[i]);
+    }
+    printf("\n\n");
+
     free(st.tree);
     
     printf("Tempo de execucao: %.6f segundos\n", elapsed_time);
 }
 
 int main() {
-    //defaultInput();
-    customInput();
+    defaultInput();
+    // customInput();
     return 0;
 }
